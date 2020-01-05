6a0909b2af252b403959905324382d92978be115
KikeyMeenan <mikemk309@googlemail.com>
2020-01-05 06:04:07 +0000

    added csharp project

--- javascript/solutions/TennisGame1.js
+++ javascript/solutions/TennisGame1.js
@@ -1,11 +1,11 @@
-var TennisGame1 = function(player1Name, player2Name) {
+var TennisGame1 = function (player1Name, player2Name) {
     this.m_score1 = 0;
     this.m_score2 = 0;
     this.player1Name = player1Name;
     this.player2Name = player2Name;
 };
 
-TennisGame1.prototype.wonPoint = function(playerName) {
+TennisGame1.prototype.wonPoint = function (playerName) {
     if (playerName === this.player1Name)
         this.m_score1 += 1;
     else
@@ -21,16 +21,16 @@ var ScoreToWords = function (score) {
     ][score]
 }
 
-var isEvenScore = function(score1, score2){
+var isEvenScore = function (score1, score2) {
     return score1 === score2
 }
 
-var isAdvantageScore = function(score1, score2){
+var isAdvantageScore = function (score1, score2) {
     return score1 >= 4 || score2 >= 4
 }
 
 var GetEvenScoreInWords = function (score) {
-    if(score >= 3){
+    if (score >= 3) {
         return "Deuce"
     }
     return ScoreToWords(score) + "-All"
@@ -43,17 +43,17 @@ var GetAdvantageScoreInWords = function (minusResult) {
     else return "Win for player2";
 }
 
-var GetScoreInWords = function(score1, score2) {
+var GetScoreInWords = function (score1, score2) {
     return ScoreToWords(score1) + "-" + ScoreToWords(score2)
 }
 
-TennisGame1.prototype.getScore = function() {
+TennisGame1.prototype.getScore = function () {
     if (isEvenScore(this.m_score1, this.m_score2)) {
         return GetEvenScoreInWords(this.m_score1)
-    } 
+    }
     if (isAdvantageScore(this.m_score1, this.m_score2)) {
         return GetAdvantageScoreInWords(this.m_score1 - this.m_score2)
-    } 
+    }
     return GetScoreInWords(this.m_score1, this.m_score2)
 };
 
